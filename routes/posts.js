069529f0d135e2cd4d239dfb54c1148f348b913f
namu56 <swi920224@gmail.com>
2023-04-18 16:33:07 +0900

    게시글 수정 API
    게시글 수정 API 완

--- routes/posts.js
+++ routes/posts.js
@@ -20,7 +20,7 @@ router.post("/posts", async (req, res) => {
   res.status(201).json({ message: "게시글을 생성하였습니다." });
 });
 
-// 게시글 조회
+// 전체 게시글 목록 조회
 
 router.get("/posts", async (req, res) => {
   try {
@@ -31,19 +31,80 @@ router.get("/posts", async (req, res) => {
     let newPosts = [];
     posts.forEach((post) => {
       let obj = {
-        postId: post._id.toString(),
+        _postId: post._id.toString(),
         user: post.user,
         title: post.title,
         createdAt: post.createdAt,
       };
-      console.log(obj);
       newPosts.push(obj);
     });
-
     res.status(200).json({ newPosts });
   } catch (err) {
     console.error(err);
     res.status(400).json({ mesage: "데이터 형식이 올바르지 않습니다." });
+  }
+});
+
+// 게시글 상세 조회
+// 제목, 작성자명, 작성 날짜, 작성 내용 조회
+
+router.get("/posts/:_postId", async (req, res) => {
+  const { _postId } = req.params;
+  console.log(_postId);
+  // 전체 조회 필요 있나? 한개만 조회하는 경우
+  const posts = await Posts.find({});
+  let newPosts = [];
+  posts.forEach((post) => {
+    let obj = {
+      _postId: post._id,
+      user: post.user,
+      title: post.title,
+      content: post.content,
+      createdAt: post.createdAt,
+    };
+    newPosts.push(obj);
+  });
+  try {
+    const [detail] = newPosts.filter(
+      (post) => String(post._postId) === String(_postId)
+    );
+    res.json({ detail });
+  } catch (err) {
+    res.status(400).json({ message: "데이터 형식이 올바르지 않습니다" });
+  }
+});
+
+//게시글 수정 API
+
+router.put("/posts/:_postId", async (req, res) => {
+  const { _postId } = req.params;
+  const { password, title, content } = req.body;
+
+  try {
+    if (!password || !title || !content) {
+      return res
+        .status(400)
+        .json({ message: "데이터 형식이 올바르지 않습니다." });
+    }
+
+    const posts = await Posts.findOne({ _id: _postId });
+
+    if (!posts) {
+      return res.status
+        .apply(404)
+        .json({ message: "게시글 조회에 실패했습니다." });
+    }
+    if (posts.password !== password) {
+      return res.status(401).json({ message: "비밀번호가 다릅니다." });
+    }
+    await Posts.updateOne(
+      { _id: _postId },
+      { $set: { title: title, content: content } }
+    );
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "서버 오류가 발생했습니다." });
   }
 });
 module.exports = router;
